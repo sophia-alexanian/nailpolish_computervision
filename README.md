9df654a3394d14d8ca12acb22efbb4e64cfeca6e
Sophie <sophia.alexanian@gmail.com>
2025-01-04 01:00:42 -0500

    tester ready

---
+++ README.md
@@ -0,0 +1,42 @@
+# Nail Style Visualizer
+
+## See what different nail shapes and colours look like on your nails!
+
+![Sample pictures of combinations](assets/Data_Flowchart.png)
+
+[Back to Top](#nail-style-visualizer)
+
+## Change style using voice commands!
+
+No need to lower your hands and type lengthy commands or scroll through different combinations to try on nails! Simply press one button and tell it what you want to see!
+
+'C' to say what colour you want (with the whole rainbow available!).
+'S' to say what shape you want (before you need to file your nails!).
+
+[Back to Top](#nail-style-visualizer)
+
+## Best of all, it's free!
+
+Try on nails for free before paying $$$ to get them done professionally or invest in a new colour of nail polish.
+
+## Sold? Here's how to use on your local machine!
+
+*Note: this tutorial assumes you have the capability to run python scripts on your local machine in terminal*
+
+*steal instructions about cloning from Robert*
+
+[Back to Top](#nail-style-visualizer)
+
+## Credits/Contact
+
+This project is dedicated to [Robert Zupancic](https://www.linkedin.com/in/robert-zupancic/), fellow UofT engineering student (and my ECE241 lab partner), who gave me the inspiration for this project.
+
+However, this project was researched, designed, and implemented by Sophia Alexanian (me!).    
+I can be reached here:  
+LinkedIn: https://www.linkedin.com/in/sophia-alexanian/   
+Email: sophia.alexanian@mail.utoronto.ca
+
+P.S. Robert and I created a piano and drum kit simulator for FPGA using Verilog for our final ECE241 project. If you're interested in seeing my work in a hardware description language (as opposed to a high-level programming language), feel free to check out our (censored for academic integrity purposes) work [here](https://github.com/RoZ4/Pianissimo-for-the-FPGA).
+
+[Back to Top](#nail-style-visualizer)
+
--- nailpainter.py
+++ nailpainter.py
@@ -1,3 +1,8 @@
+print("Please ignore any arbitrary warnings. The program will greet you when it's ready. :)")
+print(".")
+print(".")
+print(".")
+
 import mediapipe as mp
 import cv2
 import numpy as np
@@ -12,40 +17,54 @@ mp_drawing = mp.solutions.drawing_utils
 # Speech recognition setup
 recognizer = sr.Recognizer()
 
-# Function to get color from user input
+# Function for getting colour from audio!
 def get_color_from_audio():
     with sr.Microphone() as source:
-        print("Listening for a color name...")
+        print("You can choose between the following colours: red, orange, yellow, green, blue, purple, pink, black or white :)")
+        print("Calibrating for ambient noise to prepare for voice recognition... :)")
+        recognizer.adjust_for_ambient_noise(source, duration=5)  # Adjusts to background noise
+        print("Energy threshold set :)")
+        print("Listening for a color name... :)")
         try:
-            audio = recognizer.listen(source, timeout=5)
+            audio = recognizer.listen(source, timeout=15, phrase_time_limit=5)
             color_name = recognizer.recognize_google(audio).lower()
-            print(f"You said: {color_name}")
-            # Map common color names to RGB values
+            print(f"You said: {color_name} :)")
+            # Map common color names to RGB values (BGR)
             color_map = {
                 "red": (0, 0, 255),
                 "blue": (255, 0, 0),
                 "green": (0, 255, 0),
                 "yellow": (0, 255, 255),
+                "pink": (203, 192, 255),
                 "purple": (128, 0, 128),
-                "pink": (255, 105, 180),
+                "orange": (0, 165, 255),
                 "white": (255, 255, 255),
                 "black": (0, 0, 0),
-                "orange": (255, 165, 0)
             }
+            if color_name not in color_map:
+                print("That colour is not available, defaulting to green. Press 'c' to try again. :)")
             return color_map.get(color_name, (0, 255, 0))  # Default to green if not recognized
         except sr.UnknownValueError:
-            print("Sorry, I could not understand the color name.")
+            print("Sorry, I could not understand the color name, defaulting to green. Press 'c' to try again. :)")
         except sr.RequestError as e:
-            print(f"Could not request results; {e}")
+            print(f"Could not request results :(; {e}")
         except TimeoutError:
-            print("Listening timed out.")
+            print("Listening timed out. :(")
         return (0, 255, 0)  # Default color
 
 # Start capturing video
 capture = cv2.VideoCapture(0)
 
+# Capture a frame to get the dimensions (height, width)
+ret, frame = capture.read()
+height, width, _ = frame.shape  # height and width of the captured frame
+
 previousTime = 0
 color = (0, 255, 0)  # Default color (green)
+
+# Printing on terminal
+print("Welcome to our nail polish try-on software! Explore and see what style nails suit you best! :)")
+print("Press 'c' to change colour or 'q' to exit program. :)")
 
 while capture.isOpened():
     ret, frame = capture.read()
@@ -64,13 +83,26 @@ while capture.isOpened():
         for idx in [4, 8, 12, 16, 20]:  # Fingertip landmarks
             x = int(results.right_hand_landmarks.landmark[idx].x * image.shape[1])
             y = int(results.right_hand_landmarks.landmark[idx].y * image.shape[0])
-            cv2.circle(image, (x, y), 15, color, -1)
+
+            # Draw ellipse instead of a circle
+            axes = (18, 10)  
+            angle = 0  # No rotation
+            start_angle = 0
+            end_angle = 360
+            cv2.ellipse(image, (x, y), axes, angle, start_angle, end_angle, color, -1)
 
     if results.left_hand_landmarks:
         for idx in [4, 8, 12, 16, 20]:  # Fingertip landmarks
             x = int(results.left_hand_landmarks.landmark[idx].x * image.shape[1])
             y = int(results.left_hand_landmarks.landmark[idx].y * image.shape[0])
-            cv2.circle(image, (x, y), 15, color, -1)
+
+            # Draw ellipse instead of a circle
+            axes = (18, 10)  
+            angle = 0  # No rotation
+            start_angle = 0
+            end_angle = 360
+            cv2.ellipse(image, (x, y), axes, angle, start_angle, end_angle, color, -1)
+
 
     # Display FPS
     currentTime = time.time()
@@ -82,10 +114,10 @@ while capture.isOpened():
     cv2.imshow("Fingernail Detection", image)
 
     # Change color based on audio input
-    if cv2.waitKey(5) & 0xFF == ord('c'):  # Press 'c' to trigger audio input
+    key = cv2.waitKey(1) & 0xFF  # Use a small delay for key input to work
+    if key == ord('c'):  # Press 'c' to trigger audio input
         color = get_color_from_audio()
-
-    if cv2.waitKey(5) & 0xFF == ord('q'):  # Press 'q' to quit
+    elif key == ord('q'):  # Press 'q' to quit
         break
 
 # Cleanup
